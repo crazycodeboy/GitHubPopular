178411a1dfd41eb6bd4a2455a204f32890de9bac
crazycodeoby <crazycodeboy@gmail.com>
2016-06-27 22:52:50 +0800

    update

--- RepositoryDetail.js
+++ RepositoryDetail.js
@@ -19,6 +19,9 @@ var RepositoryDetail=React.createClass({
       <ScrollView >
         <View >
           <Text>{this.props.item.name}</Text>
+          <Text>{this.props.item.description}</Text>
+          <Text>{this.props.item.stargazers_count}</Text>
+          <Text>{this.props.item.owner.login}</Text>
         </View>
       </ScrollView>
     );
