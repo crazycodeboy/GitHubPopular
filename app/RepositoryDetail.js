96b56a08a3978efb3c976f923ca70d51e6dac06c
crazycodeboy <crazycodeboy@gmail.com>
2016-07-08 10:31:47 +0800

    WebView预览仓库

--- app/RepositoryDetail.js
+++ app/RepositoryDetail.js
@@ -9,6 +9,7 @@ var{
   Image,
   ScrollView,
   StyleSheet,
+  WebView,
   Text,
   View,
 }=ReactNative;
@@ -17,21 +18,26 @@ var RepositoryDetail=React.createClass({
   render:function() {
     var item=this.props.projectModel.item;
     return(
-      <ScrollView style={styles.container}>
-        <View >
-          <Text>{item.full_name}</Text>
-          <Text>{item.description}</Text>
-          <Text>{item.stargazers_count}</Text>
-          <Text>{item.owner.login}</Text>
-        </View>
-      </ScrollView>
+      // <ScrollView style={styles.container}>
+      //   <View >
+      //     <Text>{item.full_name}</Text>
+      //     <Text>{item.description}</Text>
+      //     <Text>{item.stargazers_count}</Text>
+      //     <Text>{item.owner.login}</Text>
+      //   </View>
+      // </ScrollView>
+      <WebView
+        style={styles.container}
+        source={{uri:item.html_url}}
+      />
     );
   },
 });
 const styles=StyleSheet.create({
   container: {
     flex:1,
-    backgroundColor:'#ffffff'
+    backgroundColor:'#ffffff',
+    marginBottom:50
   },
 })
 module.exports=RepositoryDetail
